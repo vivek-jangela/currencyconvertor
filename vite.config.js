dd4de8c9224890f7626c19b182bcd6ada30ca60e
Vivek jangela <vivekjangela@gmail.com>
2025-11-24 20:34:45 -1200

    fix: updates in vite config and project structure

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  
+  base: "/currencyconvertor/",
   plugins: [react(),
      tailwindcss()
-  ],
-    base: "/currencyconvertor/"
+  ]
+    
 })
